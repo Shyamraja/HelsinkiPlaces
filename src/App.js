69986c7a87be2f39619565ad98d88a5b5e760ab1
Shyam <t7bhsh00@students.oamk.fi>
2023-02-26 21:34:49 +0200

    useState, useEffect hooks and axios added to fetch the data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import PlacesTable from "./components/PlacesTable";
 
+
 function App() {
+  const [places, setPlaces] = useState([]);
+
+  const getPlaces = async () => {
+    try {
+      const res = await axios.get(`https://open-api.myhelsinki.fi/v2/places/`);
+      setPlaces(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+     getPlaces();
+  },[]);
+  
   return (
       <>
           <div className="d-flex flex-column align-items-center">
@@ -10,5 +26,5 @@ function App() {
           <PlacesTable />
       </>
   );
-}
+};
 export default App;
